240c42b52cdfd350c1f32c83bd1d8e54f8b0b5d5
dvn495 <152308538+dvn495@users.noreply.github.com>
2024-08-07 12:21:40 -0500

    feat: :construction: postData
    PostData of the entity 'proveedor'

--- API/API.js
+++ API/API.js
@@ -1,32 +1,38 @@
+const URL_API = "http://localhost:8080";
 
-const URL_API = "http://localhost:8080"
-
-const myHeaders = new Headers({
-    "Content-Type": "application/json"
-});
-
+const myHeaders = () => {
+    const token = localStorage.getItem('authToken');
+    return new Headers({
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${token}`
+    });
+};
 
 const getData = async(endpoint) => {
     try {
-        return await fetch(`${URL_API}/${endpoint}`);
-	} catch(error){
+        return await fetch(`${URL_API}/${endpoint}`, {
+            headers: myHeaders()
+        });
+    } catch(error){
         console.log(error);
-	}
-    
-}
+    }
+};
+
 const getElementData = async(endpoint, id) => {
     try {
-        return await fetch(`${URL_API}/${endpoint}/${id}`);
-	} catch(error){
+        return await fetch(`${URL_API}/${endpoint}/${id}`, {
+            headers: myHeaders()
+        });
+    } catch(error){
         console.log(error);
-	}
-    
-}
+    }
+};
+
 const updateData = async (newData, endpoint, id) => {
     try {
         const response = await fetch(`${URL_API}/${endpoint}/${id}`, {
             method: "PATCH",
-            headers: myHeaders,
+            headers: myHeaders(),
             body: JSON.stringify(newData)
         });
 
@@ -40,28 +46,36 @@ const updateData = async (newData, endpoint, id) => {
         console.error('Error en la solicitud PATCH:', error.message);
         throw error;
     }
-}
-const postData = async (datos, id) => {
+};
+
+const postData = async (datos, endpoint) => {
     try {
-        return await fetch(`${URL_API}/${id}`, {
+        const response = await fetch(`${URL_API}/${endpoint}`, {
             method: "POST",
-            headers: myHeaders,
+            headers: myHeaders(),
             body: JSON.stringify(datos)
         });
+        return response;
     } catch (error) {
         console.error('Error en la solicitud POST:', error.message);
     }
-}
+};
+
 const deleteData = async (endpoint, id) => {
     try {
         return await fetch(`${URL_API}/${endpoint}/${id}`, {
             method: "DELETE",
-            headers: myHeaders,
+            headers: myHeaders(),
         });
     } catch (error) {
         console.error('Error en la solicitud Delete:', error.message);
     }
-}
+};
+
 export {
-    deleteData as deleteData, getData as getData, postData as postDatas, updateData as updateData, getElementData as getElementData
+    deleteData,
+    getData,
+    postData,
+    updateData,
+    getElementData
 };
--- App/Components/Proveedor/proveedor.js
+++ App/Components/Proveedor/proveedor.js
@@ -1,7 +1,10 @@
+import { deleteData, getData, postData, updateData, getElementData } from '/API/API.js';
+
 export class ProveedorMenu extends HTMLElement {
     constructor(){
         super();
         this.render();
+        this.controllerAction();
     }
 
     render() {
@@ -12,16 +15,15 @@ export class ProveedorMenu extends HTMLElement {
             </div>
 
             <div class="container-titulos_list">
-                <h1 class="titulo-list">Proovedores</h1>
+                <h1 class="titulo-list">Proveedores</h1>
 
-                <a id="btnProovedores" class="button-new" href="#"><box-icon name='plus' color="#FFF"></box-icon> New supplier</a>
+                <a id="btnCreate_Proveedores" class="button-new" href="#"><box-icon name='plus' color="#FFF"></box-icon> New supplier</a>
             </div>
 
             <div class="cont-list_big">
                 <div class="cont-list">
                     <p class="titulo-list">ID</p>
-                    <p class="titulo-list">Telefono</p>
-                    <p class="titulo-list">Ciudad</p>
+                    <p class="titulo-list">Nombre</p>
                 </div>
 
                 <div class="elements-list">
@@ -31,9 +33,9 @@ export class ProveedorMenu extends HTMLElement {
                     <p class="card-text">AAA</p>
 
                     <div class="card-buttons_container">
-                        <a href="" class="card-button"><box-icon name='info-circle' color='#000' ></box-icon></a>
-                        <a href="" class="card-button"><box-icon name='trash' color='#e00000' ></box-icon></a>
-                        <a href="" class="card-button"><box-icon name='pencil' color='#02a0ff' ></box-icon></a>
+                        <a id="btnInfo_Proveedores" href="" class="card-button"><box-icon name='info-circle' color='#000' ></box-icon></a>
+                        <a id="btnDelete_Proveedores" href="" class="card-button"><box-icon name='trash' color='#e00000' ></box-icon></a>
+                        <a id="btnUpdate_Proveedores" href="" class="card-button"><box-icon name='pencil' color='#02a0ff' ></box-icon></a>
                     </div>
                 </div>
 
@@ -42,6 +44,61 @@ export class ProveedorMenu extends HTMLElement {
 
         </section>
         `;      
+    }
+
+    findAll(){
+
+    }
+
+    controllerAction() {
+        
+
+        const btnCreateProveedor = this.querySelector("#btnCreate_Proveedores")
+        const btnDeleteProveedor = this.querySelector("#btnDelete_Proveedores")
+        const btnUpdateProveedor = this.querySelector("#btnUpdate_Proveedores")
+        const btnInfoProveedor = this.querySelector("#btnInfo_Proveedores")
+
+        btnCreateProveedor.addEventListener("click", () =>{
+            this.innerHTML = /* html */ `
+            <section id="proveedor--create">
+                <div>
+                    <h2>Registre proveedor</h2>
+                </div>
+                <div>
+                    <h3>Nombre</h3>
+                    <input placeholder="proveedor" id="name">
+                </div>
+                <div>
+                    <button id="saveProveedor">Guardar</button>
+                </div>
+            </section>
+            `;
+
+            const btnSave = this.querySelector("#saveProveedor")
+            btnSave.addEventListener("click", () => {
+                const endpoint = "proveedor"
+                const nombre = this.querySelector("#name").value;
+
+                const newProveedor = {
+                    id : 0,
+                    nombre: nombre
+                }
+
+                postData(newProveedor, endpoint)
+                    .then(response => {
+                        if (response.ok){
+                            return response.json();
+                        }
+                        throw new Error('Error al crear el proveedor')
+                    })
+                    .then(data => {
+                        console.log('Proveedor creado', data);
+                    })
+                    .catch(error => {
+                        console.error('Error', error);
+                    })
+            })
+        })
     }
 
     
